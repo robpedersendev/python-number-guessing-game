40159537ccc6595ad6c46f8e8aaa9b03ee01bcd9
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2020-02-14 00:33:38 -0700

    Instantiated python project with project files

---
+++ main.py
@@ -0,0 +1,30 @@
+import random
+import sys
+
+
+def guessingGame():
+    while true:
+
+        try:
+            guess = int(
+                input(f"Number guesser.\t\t\n {name} choose a number between {start} and {end}. Enter guess? : "))
+        except (TypeError, ValueError):
+            print("Im sorry, please try again")
+            continue
+        finally:
+            print(f"{name}, your last successful guess was {guess}")
+
+        if guess != number:
+            print("\n\nNice try, keep going\n\n")
+        else:
+            print("\n\nCongrats, you chose the right number!\n\n")
+            break
+
+
+if __name__ == "__main__":
+    true = True
+    start = int(sys.argv[1])
+    end = int(sys.argv[2])
+    number = random.randrange(start, end)
+    name = input("\t\t\t\t\nHello stranger, whats your name? ")
+    guessingGame()
